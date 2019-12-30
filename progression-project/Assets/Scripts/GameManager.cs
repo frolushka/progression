2249ed77c1a3492df67a43643209874ae1ebea71
OUT-Frolov-MP <Frolov.M.P@sberbank.ru>
2019-12-30 13:46:47 +0300

    Initial commit

---
+++ progression-project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float rotationTime = .5f;
+    [SerializeField] private Transform back;
+    [SerializeField] private Transform circles;
+
+    private readonly WaitForFixedUpdate fu = new WaitForFixedUpdate(); 
+    
+    private bool isRotating;
+
+    private float times;
+    private float angle; 
+
+    private void Start()
+    {
+        times = rotationTime / Time.fixedDeltaTime;
+        angle = 30 / times; 
+    }
+
+    void Update()
+    {
+        if (isRotating) return;
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(Rotation(1));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine(Rotation(-1));
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            circles.Rotate(0, 0, angle);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            circles.Rotate(0, 0, -angle);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            circles.rotation = Quaternion.identity;
+        }
+    }
+
+    IEnumerator Rotation(float multiplier)
+    {
+        isRotating = true;
+        for (int i = 0; i < times; i++)
+        {
+            back.Rotate(0, 0, angle * multiplier);
+            yield return fu;
+        }
+        isRotating = false;
+    }
+}
